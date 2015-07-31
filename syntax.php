886ba1352145870fe1a66f0176d480454745a923
Christopher Smith <chris@jalakai.co.uk>
2015-07-31 17:30:27 +0100

    bring source up-to-date: more relaxed syntax & keywords to control iframe appearance

--- syntax.php
+++ syntax.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Iframe: Inserts an iframe element to include the specified url
- * 
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Christopher Smith <chris@jalakai.co.uk>
  */
@@ -10,65 +10,98 @@ if(!defined('DOKU_INC')) die();
 
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
- 
+
 /**
  * All DokuWiki plugins to extend the parser/rendering mechanism
  * need to inherit from this class
  */
 class syntax_plugin_iframe extends DokuWiki_Syntax_Plugin {
- 
+
     function getInfo(){
       return array(
         'author' => 'Christopher Smith',
         'email'  => 'chris@jalakai.co.uk',
-        'date'   => '2008-08-13',
-        'name'   => 'Iframe Plugin',
+        'date'   => '2008-10-31',
+        'name'   => 'iframe Plugin',
         'desc'   => 'Add an iframe containing the specified url
-                     syntax: {{url>http://www.somesite.com/somepage.htm[w,h]|alternate text}}',
+                     syntax: {{url>http://www.somesite.com/somepage.htm [w,h]|alternate text}}',
         'url'    => 'http://www.dokuwiki.org/plugin:iframe',
       );
     }
- 
+
     function getType() { return 'substition'; }
     function getSort() { return 305; }
     function connectTo($mode) { $this->Lexer->addSpecialPattern('{{url>.*?}}',$mode,'plugin_iframe'); }
-    
-    function handle($match, $state, $pos, &$handler){
-      $match = html_entity_decode(substr($match, 6, -2));
-      @list($url, $alt) = explode('|',$match,2);
-      $matches=array();
-// '/^\s*([^\[|]+)(?:\[(?:([^,\]]*),)?([^,\]]*)\])?(?:\s*(?:\|\s*(.*))?)?$/mD'
-      if (preg_match('/(.*)\[(.*)\]$/',trim($url),$matches)) {
-        $url = $matches[1];
-        if (strpos($matches[2],',') !== false) {
-          @list($w, $h) = explode(',',$matches[2],2);
-        } else {
-          $h = $matches[2];
-          $w = '98%';
-        }
-      } else {
-        $w = '98%';
-        $h = '400px';
-      }
-      
-      if (!isset($alt)) $alt = '';
-      if (!$this->getConf('js_ok') && substr($url,0,11) == 'javascript:') $url = 'error';
 
-      return array(hsc(trim($url)), hsc(trim($alt)), hsc(trim($w)), hsc(trim($h))); 
+    function handle($match, $state, $pos, &$handler){
+        $match = substr($match, 6, -2);
+        list($url, $alt)   = explode('|',$match,2);
+        list($url, $param) = explode(' ',$url,2);
+
+        // javascript pseudo uris allowed?
+        if (!$this->getConf('js_ok') && substr($url,0,11) == 'javascript:'){
+            $url = false;
+        }
+
+        // set defaults
+        $opts = array(
+                    'url'    => $url,
+                    'width'  => '98%',
+                    'height' => '400px',
+                    'alt'    => $alt,
+                    'scroll' => true,
+                    'border' => true,
+                    'align'  => false,
+                );
+
+        // handle size parameters
+        $matches=array();
+        if(preg_match('/\[?(\d+(em|%|pt|px)?)\s*([,xX]\s*(\d+(em|%|pt|px)?))?\]?/',$param,$matches)){
+            if($matches[4]){
+                // width and height was given
+                $opts['width'] = $matches[1];
+                if(!$matches[2]) $opts['width'] .= 'px'; //default to pixel when no unit was set
+                $opts['height'] = $matches[4];
+                if(!$matches[5]) $opts['width'] .= 'px'; //default to pixel when no unit was set
+            }elseif($matches[2]){
+                // only height was given
+                $opts['height'] = $matches[1];
+                if(!$matches[2]) $opts['height'] .= 'px'; //default to pixel when no unit was set
+            }
+        }
+
+        // handle other parameters
+        if(preg_match('/noscroll(bars?|ing)?/',$param)){
+            $opts['scroll'] = false;
+        }
+        if(preg_match('/no(frame)?border/',$param)){
+            $opts['border'] = false;
+        }
+        if(preg_match('/(left|right)/',$param,$matches)){
+            $opts['align'] = $matches[1];
+        }
+
+        return $opts;
     }
-    
-    function render($mode, &$renderer, $data) {
-    
-      list($url, $alt, $w, $h) = $data;
-      if($mode == 'xhtml'){
-          if ($url != 'error') {
-					  $renderer->doc .= '<p>Click <a href="'.$url.'" alt="'.$alt.'" target="_new">here</a> to open the page in its own window.</p>';
-            $renderer->doc .= '<iframe title="'.$alt.'" src="'.$url.'" style="width:'.$w.'; height: '.$h.';">'.$alt.'</iframe>';
-          } else {
-            $renderer->doc .= '<div>'.$alt.'</div>';
-          }
-          return true;
-      }
-      return false;
+
+    function render($mode, &$R, $data) {
+        if($mode != 'xhtml') return false;
+
+        if(!$data['url']){
+            $R->doc .= '<div class="iframe">'.hsc($data['alt']).'</div>';
+        }else{
+            $opts = array(
+                        'title' => $data['alt'],
+                        'src'   => $data['url'],
+                        'style' => 'width:'.$data['width'].'; height:'.$data['height'],
+                        );
+            if(!$data['border']) $opts['frameborder'] = 0;
+            if(!$data['scroll']) $opts['scrolling'] = 'no';
+            if($data['align'])   $opts['align'] = $data['align'];
+            $params = buildAttributes($opts);
+            $R->doc .= "<iframe $params>".hsc($alt).'</iframe>';
+        }
+
+        return true;
     }
 }
